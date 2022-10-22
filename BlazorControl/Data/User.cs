cb6c6d74a7f9fe8b8bc82bacbfd0e073168c1853
202010 <202010@1220-004-22>
2022-10-22 08:55:52 +0300

    Добавьте файлы проекта.

---
+++ BlazorControl/Data/User.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace BlazorControl.Data
+{
+    public class User
+    {
+        [BsonId]
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MongoDBLink/MongoDB.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazorControl.Data;
+
+namespace MongoDBLink
+{
+    public static class MongoDB
+    {
+        public static void AddToDataBase(User user)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("BlazorControlZaripov");
+            var collection = database.GetCollection<User>("UserCollection");
+            collection.InsertOne(user);
+        }
+
+        public static User FindById(string id)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var filter = Builders<User>.Filter.Eq("_id", ObjectId.Parse(id));
+            var database = client.GetDatabase("BlazorControlZaripov");
+            var collection = database.GetCollection<User>("UserCollection");
+            return collection.Find(filter).FirstOrDefault();
+        }
+
+
+        public static IMongoCollection<User> GetCollection()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("BlazorControlZaripov");
+
+            return  database.GetCollection<User>("UserCollection");
+        }
+    }
+}
